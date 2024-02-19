292fb8a5954b98c6fc5ae58cd0a7eaee505c31e6
mikki <2930017F@student.gla.ac.uk>
2024-02-19 14:52:01 +0000

    I made my stuff to test and debug but also try to play around Changes to be committed:

--- app/structures/basic/Board.java
+++ app/structures/basic/Board.java
@@ -17,6 +17,7 @@ public class Board {
             }
         }
 
+
     }
 
     public Tile getTile(int x, int y) {
--- app/structures/basic/Hand.java
+++ app/structures/basic/Hand.java
@@ -1,17 +1,28 @@
 package structures.basic;
 
+import java.util.ArrayList;
+
 public class Hand {
-    private Card[] cards;
+	
+    private ArrayList<Card> cards;
     private int size;
 
     public Hand() {
-        this.cards = new Card[6];
+        this.cards = new ArrayList<>();
         this.size = 0;
     }
 
-    public void draw(Card card) {
-
+    public void addCard(Card drawnCard) {
+        cards.add(drawnCard);
+        size++;
     }
-
+    public Card getCardAtIndex(int index) {
+        if (index >= 0 && index < size) {
+            return cards.get(index);
+        } else {
+            return null; 
+        }
+    }
 }
+
 
