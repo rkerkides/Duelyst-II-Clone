000021a09f03aad7ae3ba8ef4c29690c8b01d0b7
Emran Ramezan <Emran@opulence.design>
2024-03-10 14:55:21 +0000

    initial stats

--- test/HumanPlayerTests.java
+++ test/HumanPlayerTests.java
@@ -1,6 +1,11 @@
+import static org.junit.Assert.assertEquals;
+
 import org.junit.Test;
 
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 import events.Initalize;
+import play.libs.Json;
 import structures.GameState;
 
 /**
@@ -9,13 +14,17 @@ import structures.GameState;
 public class HumanPlayerTests {
 
   @Test
-  public void checkHealth() {
+  public void initialStats() {
 
     GameState gameState = new GameState(); // create state storage
     Initalize initalizeProcessor = new Initalize(); // create an initalize event processor
 
     ObjectNode eventMessage = Json.newObject(); // create a dummy message
     initalizeProcessor.processEvent(null, gameState, eventMessage); // send it to the initalize event processor
+
+    assertEquals(gameState.getHuman().getHealth(), 20 );
+    assertEquals(gameState.getHuman().getMana(), 2);
+    assertEquals(gameState.getHuman().getHand().getNumberOfCardsInHand(), 3);
 
   }
 
