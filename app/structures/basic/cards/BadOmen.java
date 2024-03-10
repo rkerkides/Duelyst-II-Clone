29b3fe8e75e77727329f1f26f68cac8707c48faf
mikki <2930017F@student.gla.ac.uk>
2024-03-10 16:14:12 +0000

    fixed deathwatch bug

--- app/structures/basic/cards/BadOmen.java
+++ app/structures/basic/cards/BadOmen.java
@@ -17,27 +17,11 @@ import static utils.StaticConfFiles.card_badOmen;
 
 public class BadOmen extends Unit {
 
-    int BadOmenCount = 0;
-
-
     // method to check if bad omen is onthe board and increment attack each time someone dies.
     public static void BadOmenDeathwatch( ActorRef out, GameState gameState, GameService gs) {
         for (Unit unit : gameState.getUnits()) {
             if (unit.getName().equals("Bad Omen")) {
-                gameState.removeFromTotalUnits(1); // method that removes one unit from the total count
-                int newAttack = unit.getAttack() + 1;
-                //BasicCommands.setUnitAttack(out, unit, unit.getAttack() + 1); //inceremnt attack if true;
-                gs.updateUnitAttack(unit, newAttack);
-                break;
-            }
-        }
-
-        System.out.println("Units on the board: " + gameState.getUnitsOnBoard().size());
-
-        try {
-            Thread.sleep(500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+                gs.updateUnitAttack(unit, unit.getAttack() + 1);            }
         }
 
     }
