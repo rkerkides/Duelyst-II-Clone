bd6b770a528a02348cc060dc5af411d318cd0eb7
luciantkd <lucianprocopciuc1@hotmail.com>
2024-03-10 19:52:37 +0000

    fixed ai mana initiation

--- app/structures/GameState.java
+++ app/structures/GameState.java
@@ -80,6 +80,8 @@ public class GameState {
 
 		// Player mana initialised to 2
 		gameService.updatePlayerMana(human, 2);
+		gameService.updatePlayerMana(ai, 2);
+		System.out.println(getAi().getMana());
 
 		// Create the human and AI avatars
 		gameService.loadAvatar(board, human);
